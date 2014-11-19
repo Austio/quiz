bfa578008c554ed521701e424f31c1c864fa006e
Austio@windowslaptop <lonnieastory@gmail.com>
2014-11-18 21:31:47 -0600

    Removed answer from link to add field

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -9,7 +9,7 @@ module QuizHelper
     end
   end
 
-  def link_to_add_answer_field(name, f, association)
+  def link_to_add_field(name, f, association)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
